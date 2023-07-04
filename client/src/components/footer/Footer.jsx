e79e9fedd5360f7582d059bfdb54646bf2e72b58
Birhane Kahsay <birhane.kahsay99@gmail.com>
2023-07-05 00:06:32 +0200

    rebranding

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../footer/footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHashtag, faHelmetSafety } from '@fortawesome/free-solid-svg-icons'
+import { NavLink } from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='footers'>
@@ -13,29 +14,32 @@ const Footer = () => {
                 <h4>Full stack developer</h4>
                 <p>birhane.kahsayw@gmail.com</p>
         </section>
+
         <section className='footer-contact'>
             <h2 className='footer__header'>Contact me</h2>
             <form className='contact'>
                 <div className='contact__details'>
-                    <label htmlFor="name">Name</label>
+                    <label htmlFor="name">Name <i className='must'>*</i></label>
                     <input type="text" name="name" id="name" />
                 </div>
                 <div className='contact__details'>
-                    <label htmlFor="email">email</label>
+                    <label htmlFor="email">Email <i className='must'>*</i></label>
                     <input type="email" name="email" id="email" />
                 </div>
                 <div className='contact__details'>
-                    <label htmlFor="message">message</label>
+                    <label htmlFor="message">Message <i className='must'>*</i></label>
                     <textarea type="text" name="message" id="message" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
         </section>
+
         <section className='footer-social'>
             <h2 className='footer__header'>You can also follow me on</h2>
             <div className='social'>
-                <FontAwesomeIcon icon={faHashtag} />
-                <FontAwesomeIcon icon={faHelmetSafety} />
+                <NavLink to='https://github.com/Birhanek/arho'><FontAwesomeIcon className='twitter' icon={faHelmetSafety} /></NavLink>
+                <NavLink to='https://twitter.com/'><FontAwesomeIcon icon={faHashtag} /></NavLink>
+                
             </div>
         </section>
         </div>
